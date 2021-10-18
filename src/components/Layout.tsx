c7a2806ffc972e714de0c9e821a83359ccdcedb5
Isack <ituwei21@gmail.com>
2021-10-19 00:25:12 +0300

    added components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import {ReactNode} from 'react';
+import {Flex} from '@chakra-ui/react';
+type props= {
+    children: ReactNode;
+};
+
+
+export default function Layout({children}:props){
+
+    return (
+
+        <Flex 
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        h="100vh"
+        bg="gray.800"
+        >
+            {children}
+        </Flex>
+    )
+}
